275975ba28aeb19da295d8ff017ab1c71d80b483
Tagha Emmaculate Shie <taghashie@gmail.com>
2022-09-02 14:48:12 +0100

    Adjusted the form in HTML to enable me search for City

--- Src/Index.js
+++ Src/Index.js
@@ -1,9 +1,7 @@
 function formatDate(timestamp) {
-    //let date = new Date(timestamp);
-    let currentTime = new Date();
+    let date = new Date(timestamp);
     let hours = date.getHours();
-    let minutes = date.getMinutes;
-     let day = date.getDay();
+    let minutes = date.getMinutes();
     if (hours < 10) {
         hours = `0${hours}`;
     }
@@ -19,37 +17,43 @@ function formatDate(timestamp) {
         "Friday",
         "Saturday"
     ];
-   
-
-    
+    let day = days[date.getDay()];
     return `${day} ${hours}:${minutes}`
 }
 
 
-function showTemperature(response) {
+function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temp");
     let cityElement = document.querySelector("#city");
     let humidityElement = document.querySelector("#hum");
     let windElement=document.querySelector("#wind")
-    let dateElement = document.querySelector("#time");
+    let dateElement = document.querySelector("#date");
     let iconElement = document.querySelector("#icon")
-    //let DateElement = document.querySelector("#date");
     let weatherCondition = document.querySelector("#weather-cond");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     weatherCondition.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
-    //DateElement.innerHTML = formatDate(response.data.dt * 1000);
     iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt", response.data.weather[0].description);
-    dateElement.innerHTML = formatDate(currentTime);
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
     windElement.innerHTML = Math.round(response.data.wind.speed);
-
-
-
 }
+
+function search(city) {
 let apiKey = "1198758847d63b62f48537f6840537aa";
-let city="Abuja"
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
-    axios.get(apiUrl).then(showTemperature);
+
+axios.get(apiUrl).then(displayTemperature); 
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement= document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+search("Yaounde");
