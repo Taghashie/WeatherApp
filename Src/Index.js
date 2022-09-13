59ba679bb8039c932079e9903a8bba6313a4076a
Tagha Emmaculate Shie <taghashie@gmail.com>
2022-09-13 04:27:48 +0100

    Integrated the weather API to get the daily weather forecast with maximum and minimum temperature to update with the city search

--- Src/Index.js
+++ Src/Index.js
@@ -21,35 +21,42 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`
 }
 
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+    return days[day];
+}
 
 function displayForecast(response) {
-    console.log(response.data.daily);
+    let forecast=response.data.daily;
     let forecastElement=document.querySelector("#forecast");
     let forecastHTML = `<div class="row">`;
-    let days = ["Thur", "Fri", "Sat", "Sun","Mon"];
-    days.forEach(function (day) {
-   forecastHTML= forecastHTML +
-   `
+    forecast.forEach(function (forecastDay, index) {
+        if (index < 6) { 
+        forecastHTML = forecastHTML +
+            `
             <div class="col-2">
-                <div class="weather-forecast-date">${day}</div>
-                <img src="http://openweathermap.org/img/wn/50d@2x.png"
+                <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+                <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
                 alt=""
                 width="42"
                 />
                 <div class="weather-forecast-temp">
-                    <span class="weather-forecast-temp-max">18</span>
-                    <span class="weather-forecast-temp-min">15</span>
+                    <span class="weather-forecast-temp-max">${Math.round(forecastDay.temp.max)} °</span>
+                    <span class="weather-forecast-temp-min">${Math.round(forecastDay.temp.min)} °</span>
                 </div>
             </div>
-    `;       
-    })
+    `;
+    };   
+    });
   
    forecastHTML=forecastHTML+ `</div>`;
    forecastElement.innerHTML=forecastHTML;
 }
 
 function getForecast(coordinates) {
-    console.log(coordinates);
+    //console.log(coordinates);
     let apiKey = "1198758847d63b62f48537f6840537aa";
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayForecast);
@@ -120,4 +127,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Bamenda");
-displayForecast();
+//displayForecast();
