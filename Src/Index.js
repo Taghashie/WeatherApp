8bea0ae02d9e23a1b52f0ef57f0d66998e2322f0
Tagha Emmaculate Shie <taghashie@gmail.com>
2022-09-18 18:24:20 +0100

    Made finishing touches to the app by deleting the Fahrenheit temperature conversion function.

--- Src/Index.js
+++ Src/Index.js
@@ -56,7 +56,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-    //console.log(coordinates);
     let apiKey = "1198758847d63b62f48537f6840537aa";
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayForecast);
